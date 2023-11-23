98b84380d7d5684910b3928160b477b92e89a52b
Sam Taylor <70077131+sht90@users.noreply.github.com>
2023-11-23 09:58:52 -0500

    make rowcol class

---
+++ SplitDecisions/SplitDecisions/RowCol.cs
@@ -0,0 +1,18 @@
+﻿namespace SplitDecisions
+{
+    /// <summary>
+    /// Literally just a tuple for Row, Column
+    /// </summary>
+    public class RowCol
+	{
+		public int Row;
+		public int Col;
+
+		public RowCol(int row, int col)
+		{
+			Row = row;
+			Col = col;
+		}
+	}
+}
+
