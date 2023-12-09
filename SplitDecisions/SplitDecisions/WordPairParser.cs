c920f0f80ba09b9a87e1db1929801d2208ecbca3
Sam Taylor <70077131+sht90@users.noreply.github.com>
2023-12-09 17:49:31 -0500

    finally finish wordpairparser

--- SplitDecisions/SplitDecisions/WordPairParser.cs
+++ SplitDecisions/SplitDecisions/WordPairParser.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics.Metrics;
-
-namespace SplitDecisions
+﻿namespace SplitDecisions
 {
     internal static class WordPairParser
 	{
@@ -20,15 +18,64 @@ namespace SplitDecisions
             int openParenIndex = parts[0].IndexOf('(');
             int slashIndex = parts[0].IndexOf('/');
             int closeParenIndex = parts[0].IndexOf(')');
-            string before = parts[0].Substring(0, openParenIndex);
-            string split1 = parts[0].Substring(openParenIndex + 1, slashIndex - (openParenIndex + 1));
-            string split2 = parts[0].Substring(slashIndex + 1, closeParenIndex - (slashIndex + 1));
-            string after = parts[0].Substring(closeParenIndex + 1, parts[0].Length - (closeParenIndex + 1));
+            string before = parts[0][..openParenIndex];
+            string split1 = parts[0][(openParenIndex + 1)..slashIndex];
+            string split2 = parts[0][(slashIndex + 1)..closeParenIndex];
+            string after = parts[0][(closeParenIndex + 1)..];
             // technically this is enough to get a wordPair, but it won't have the anchors or mistakeables yet
             WordPair wp = new(new Shape(before.Length + after.Length + 2, before.Length), before + split1 + after, before + split2 + after, 2);
-            // TODO:
             // parse the mistakeables
+            // trim off outer brackets from mistakeables portion
+            parts[1] = parts[1][2..^2];
+            List<int> mistakeables = new() { };
+            // now, for each new set of brackets, make an int to store all the letters
+            foreach (char c in parts[1])
+            {
+                if (c == '[')
+                {
+                    mistakeables.Add(0);
+                    continue;
+                }
+                else if (c == ']' || c == ' ')
+                {
+                    continue;
+                }
+                mistakeables[^1] += LetterCode.Encode(c);
+            }
+            // finally add mistakeables to the wordpair
+            wp.Mistakeables = mistakeables;
             // parse the anchor conditions
+            // trim off outer brackets from anchor portion
+            parts[2] = parts[2][2..^2];
+            List<List<bool>> anchors = new() { };
+            // now, for each new set of brackets, make a list of bools
+            foreach (char c in parts[2])
+            {
+                if (c == '[')
+                {
+                    anchors.Add(new List<bool>() { });
+                    continue;
+                }
+                else if (c == ']' || c == ' ')
+                {
+                    continue;
+                }
+                // I feel like there's a more compact way to write this
+                if (c == '0')
+                {
+                    anchors[^1].Add(false);
+                }
+                else if (c == '1')
+                {
+                    anchors[^1].Add(true);
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+            // add anchors to wordpair
+            wp.Anchors = anchors;
             return wp;
 		}
 
@@ -39,7 +86,7 @@ namespace SplitDecisions
             List<WordPair> retVal = new() { };
             while ((line = file.ReadLine()) != null)
             {
-                retVal.Append(Parse(line));
+                retVal.Add(Parse(line));
             }
             return retVal;
         }
