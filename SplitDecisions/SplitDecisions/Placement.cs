0bd18b884241ebca035e96d85c25aa57adf0538a
Sam Taylor <70077131+sht90@users.noreply.github.com>
2024-04-13 19:13:25 -0400

    Add placement and rowcol classes

---
+++ SplitDecisions/SplitDecisions/Placement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace SplitDecisions
+{
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical
+    }
+
+    public class Placement
+    {
+        public int Row;
+        public int Col;
+        public Orientation Dir;
+        public Placement(int row, int col, Orientation horizontal)
+        {
+            Row = row;
+            Col = col;
+            Dir = horizontal;
+        }
+
+        public override string ToString()
+        {
+            return Dir == Orientation.Horizontal ? string.Format("{0},{1} Across", Row, Col) : string.Format("{0},{1} Down  ", Row, Col);
+        }
+
+        public RowCol LastLetterRowCol(int length, BoardSettings settings)
+        {
+            if (Dir == Orientation.Horizontal) return new RowCol(Row, Col + length, settings);
+            return new RowCol(Row + length, Col, settings);
+        }
+    }
+}
+
