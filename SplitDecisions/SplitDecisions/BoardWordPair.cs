c63b6c308fa9079b5043faefe26d9168fb8451e6
Sam Taylor <70077131+sht90@users.noreply.github.com>
2024-04-14 15:17:04 -0400

    Create BoardWordPair class

---
+++ SplitDecisions/SplitDecisions/BoardWordPair.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace SplitDecisions
+{
+	public class BoardWordPair
+	{
+		public Placement Placement;
+		public WordPair WordPair;
+		public int Intersections;
+
+		public BoardWordPair(Placement placement, WordPair wordPair)
+		{
+			Placement = placement;
+			WordPair = wordPair;
+			Intersections = 0;
+		}
+	}
+}
+
