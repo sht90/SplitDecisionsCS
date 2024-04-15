107a0488b16146f7dca5b27ee9b3082be6669db0
Sam Taylor <70077131+sht90@users.noreply.github.com>
2024-04-15 16:56:42 -0400

    give BoardWordPair All PossibleAnchors field

--- SplitDecisions/SplitDecisions/BoardWordPair.cs
+++ SplitDecisions/SplitDecisions/BoardWordPair.cs
@@ -6,12 +6,20 @@ namespace SplitDecisions
 		public Placement Placement;
 		public WordPair WordPair;
 		public int Intersections;
+		public List<bool> AllPossibleAnchors;
 
 		public BoardWordPair(Placement placement, WordPair wordPair)
 		{
 			Placement = placement;
 			WordPair = wordPair;
 			Intersections = 0;
+			AllPossibleAnchors = Enumerable.Repeat(false, wordPair.Letters.Length).ToList();
+			int allPossibleAnchorsInt = 0;
+			foreach (int anchor in WordPair.Anchors) { allPossibleAnchorsInt |= anchor; }
+			for (int i = 0; i < AllPossibleAnchors.Count; i++)
+			{
+				AllPossibleAnchors[i] = (allPossibleAnchorsInt & 1 << WordPair.Letters.Length - 1 - i) > 0;
+			}
 		}
 	}
 }
